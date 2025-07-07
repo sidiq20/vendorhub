e6e6dba3f450e7488cf7c5fcd501dbe9b0d76582
sidiq20 <sidiqolasode@gmail.com>
2025-07-07 23:54:30 +0100

    new changes

---
+++ models/products.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+import uuid
+
+class Product:
+    class __init__
---
+++ models/user.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Optional
+import uuid
+
+
+class User:
+    def __init__(self, firebase_uid: str, name: str, email: Optional[str] = None, phone: Optional[str] = None,
+                 store_name: Optional[str] = None, store_slug: Optional[str] = None,
+                 store_whatsapp: Optional[str] = None, store_description: Optional[str] = None):
+        self.id = str(uuid.uuid4())
+        self.firebase_uid = firebase_uid
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        self.store_name = store_name
+        self.store_slug = store_slug
+        self.store_whatsapp = store_whatsapp
+        self.store_description = store_description
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'firebase_uid': self.firebase_uid,
+            'email': self.email,
+            'phone': self.phone,
+            'name': self.name,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            'store': {
+                'name': self.store_name,
+                'slug': self.store_slug,
+                'whatsapp': self.store_whatsapp,
+                'description': self.store_description
+            }
+        }
