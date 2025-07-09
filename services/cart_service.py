16e1e4c6d497921eb851dc33344e304ee74f15c3
sidiq20 <sidiqolasode@gmail.com>
2025-07-09 22:04:34 +0100

    new updates

--- services/cart_service.py
+++ services/cart_service.py
@@ -34,3 +34,27 @@ def get_or_create_cart(db, session):
     return cart
 
 def calculate_cart_items(db, cart):
+    cart_items = []
+    total = 0
+    
+    for item in cart.get('items', []):
+        product = db.products.find_one({"_id": ObjectId(item["product_id"])})
+        if product:
+            store = db.users.find_one(
+                {"_id": ObjectId(product["user_id"])},
+                {
+                    "store.name":1,
+                    "store.slug": 1
+                }
+            )
+            item_total = product["price"] * item["quantity"]
+            total += item_total
+            
+            cart_items.append({
+                "product": product,
+                "store": store["store"] if store else {},
+                "quantity": item["quantity"],
+                "item_total": item_total
+            })
+            
+        return cart_items, total
